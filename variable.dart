d2ca87af36d6b1f4ec4ddf307880129f1ae68a6c
AbQaadir <qaadir.inbox@gmail.com>
2024-03-22 22:58:39 +0530

    int, float, String, bool, dynamic, const, final variables

---
+++ variable.dart
@@ -0,0 +1,32 @@
+void main() {
+  // integer
+  int age = 20;
+  print(age);
+
+  // double
+  double pi = 3.14;
+  print(pi);
+
+  // string
+  String name = "John Doe";
+  print(name);
+
+  // boolean
+  bool isTrue = true;
+  print(isTrue);
+
+  // dynamic
+  dynamic dynamicVar = 10;
+  print(dynamicVar);
+
+  dynamicVar = "Hello";
+  print(dynamicVar);
+
+  // const
+  const double gravity = 9.8;
+  print(gravity);
+
+  // final
+  final double piFinal = 3.14;
+  print(piFinal);
+}
