0cae6ac84ff9f758d70b1144c6ac0629537a22ce
AbQaadir <qaadir.inbox@gmail.com>
2024-03-22 23:01:39 +0530

    data types : List, Set, Map

---
+++ datatypes.dart
@@ -0,0 +1,41 @@
+void main() {
+  // integer
+  int age = 20;
+  print(age);
+
+  // double
+  double pi = 3.14;
+  print(pi);
+
+  // string
+  String name = "John Doe";
+  print(name);
+
+  // boolean
+  bool isTrue = true;
+  print(isTrue);
+
+  // dynamic
+  dynamic dynamicVar = 10;
+  print(dynamicVar);
+
+  dynamicVar = "Hello";
+  print(dynamicVar);
+
+  // List
+  List<int> numbers = [1, 2, 3, 4, 5];
+  print(numbers);
+
+  // Set
+  Set<int> numberSet = {1, 2, 3, 4, 5};
+  print(numberSet);
+
+  // Map
+  Map<String, dynamic> user = {
+    'name': 'John Doe',
+    'age': 20,
+    'isStudent': true
+  };
+
+  print(user);
+}
